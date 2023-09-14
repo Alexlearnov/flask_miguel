0dd73772b868748c59b5cbe3d1a51718889fbd3f
Alex <Alexorierwe@protonmail.com>
2023-09-14 11:42:42 +0300

    added log_ing & log_out features

--- app/models.py
+++ app/models.py
@@ -1,21 +1,36 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class UsersView(UserMixin, db.Model):
+    __tablename__ = "Users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('PostsView', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.username} Email {self.email}>'
 
-class Post(db.Model):
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+class PostsView(db.Model):
+    __tablename__ = "Posts"
+
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
 
     def __repr__(self):
         return f'<Post {self.body}>'
+
+@login.user_loader
+def load_user(id):
+    return UsersView.query.get(int(id))
